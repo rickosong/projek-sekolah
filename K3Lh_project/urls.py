af9e620c287ebe91d0f5b40309a2d87cb9fb6916
rickosong <morito@smk2.ac.id>
2021-04-06 22:04:19 +0800

    mengimport def login, yg mengimport aadalah urls.py, dan membuat laman login

--- K3Lh_project/urls.py
+++ K3Lh_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from K3Lh_website.views import login
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login),
 ]
