a80b1896a98ba5b1f11bc57daec182eda6588759
rickosong <morito@smk2.ac.id>
2021-04-06 17:23:01 +0800

    menambahkan path home untuk sementara waktu, hanay untuk testing

--- K3Lh_project/urls.py
+++ K3Lh_project/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Testing')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', home)
 ]
