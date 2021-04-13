9a41cd3c65df4a1a3bf95c163f56a516c4e32e6c
rickosong <morito@smk2.ac.id>
2021-04-13 17:55:55 +0800

    menghubungkan models kotak ke admin

--- K3Lh_website/admin.py
+++ K3Lh_website/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from K3Lh_website.models import Kotak, Keadaan
 
 # Register your models here.
+class KotakAdmin(admin.ModelAdmin):
+    list_display = ['lokasi', 'tanggal', 'keadaan', 'keterangan']
+    search_fields =  ['lokasi', 'tanggal', 'keadaan']
+    list_filter = ('keadaan',)
+    list_per_page = 5
+
+admin.site.register(Kotak, KotakAdmin)
+admin.site.register(Keadaan)
--- K3Lh_website/models.py
+++ K3Lh_website/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Keadaan(models.Model):
-    jenis = models.CharField(max_length = 40)
+    jenis = models.CharField(max_length=40)
 
     def __str__(self):
         return self.jenis
 
 class Kotak(models.Model):
-    lokasi = models.CharField(max_length = 90)
-    tanggal = models.CharField(max_length = 35)
-    keadaan = models.ForeignKey(Keadaan, on_delete = models.CASCADE, null = True)
-    keterangan = models.TextField(blank = True)
+    lokasi = models.CharField(max_length=90)
+    tanggal = models.CharField(max_length=35)
+    keadaan = models.ForeignKey(Keadaan, on_delete = models.CASCADE, null=True)
+    keterangan = models.TextField(blank=True)
 
     def __str__(self):
         return self.lokasi
